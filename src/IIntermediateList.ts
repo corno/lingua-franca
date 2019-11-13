1b0b528e4c1bc73e4fcf5de52ffe7fc583caa5a1
corno <corno@schraverus.com>
2019-11-13 05:36:03 +0100

    added missing interface

---
+++ src/IIntermediateList.ts
@@ -0,0 +1,8 @@
+import { List } from "lingua-franca"
+
+/**
+ * IIntermediateList does not have to do anything.
+ * This interface is provided for symmetry with the IntermediateDictionary interfaces
+ */
+export interface IIntermediateList<Type> extends List<Type> {
+}
