86d76eae0dc50f82855d484dfd63211479cdc709
corno <github.a@schraverus.com>
2019-12-23 08:32:50 +0100

    exporting basic types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 export * from "./interfaces/dictionaries"
+export * from "./interfaces/Dictionary"
 export * from "./interfaces/delayedResolve"
 export * from "./interfaces/IBuildContext"
 export * from "./interfaces/IListBuilder"
 export * from "./interfaces/instantResolve"
+export * from "./interfaces/List"
+export * from "./interfaces/Reference"
 
 export { createBuildContext } from "./implementation/createBuildContext"
 
